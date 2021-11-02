f68ef74dc03dfc3e69bbd4ade78519cb5c0a2e28
Andranik <manushakyana@gmail.com>
2021-11-02 12:30:15 +0300

    Initial commit

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import {
+	Formik,
+	FormikHelpers,
+	FormikProps,
+	Form,
+	Field,
+	FieldProps,
+} from "formik";
+
+interface ContactFormValues {
+	firstName: string;
+}
+
+const ContactForm: FC<{}> = () => {
+	const initialValues: ContactFormValues = { firstName: "" };
+	return (
+		<div>
+			<h1>My Example</h1>
+			<Formik
+				initialValues={initialValues}
+				onSubmit={(values, actions) => {
+					console.log({ values, actions });
+					alert(JSON.stringify(values, null, 2));
+					actions.setSubmitting(false);
+				}}
+			>
+				<Form>
+					<label htmlFor="firstName">First Name</label>
+					<Field
+						id="firstName"
+						name="firstName"
+						placeholder="First Name"
+					/>
+					<button type="submit">Submit</button>
+				</Form>
+			</Formik>
+		</div>
+	);
+};
+
+export default ContactForm;
